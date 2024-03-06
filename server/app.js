42fc3ad3dcfcde09f09353746427eedb1d14a7d3
burakbehlull <burakyabgu@gmail.com>
2024-03-06 16:40:47 +0300

    feat: Completed user information retrieval endpoint

--- server/app.js
+++ server/app.js
@@ -1,18 +1,40 @@
 const express = require('express')
-
+const axios = require('axios')
+const {Client, GatewayIntentBits} = require('discord.js')
 require('dotenv').config()
-const {PORT} = process.env
+const {PORT, BOT_TOKEN} = process.env
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildPresences,
+        GatewayIntentBits.GuildMembers,
+		GatewayIntentBits.MessageContent,
+    ]
+})
 
 const app = express()
 
-app.post('/user', (req,res)=> {
-    const {userId} = req.body
-    
+// pages
+app.get('/user/:userId', async (req,res)=> {
+    const {userId} = req.params
+    const user = await getUser(userId)
+    const info = await getInfo(userId, BOT_TOKEN) 
+
     try {
         res.status(200).json({
             success: true,
             message: "Kullanıcı bulundu.",
-            user: ''
+            user: {
+                id: user.id,
+                username: user.username,
+                discriminator: user.discriminator,
+                avatar: user.displayAvatarURL({ format: 'png', size: 4096, dynamic: true }),
+                banner: `https://cdn.discordapp.com/banners/${userId}/${info.banner}.jpg?size=4096`,
+                u_color: info.color, 
+                created: user.createdAt,
+            }
         })
     } catch (err) {
         res.status(204).json({
@@ -23,6 +45,8 @@ app.post('/user', (req,res)=> {
     }
 })
 
+
+client.login(BOT_TOKEN)
 app.listen(process.env.PORT, ()=> {
     console.log(`Sunucu ${PORT} portunda başlatıldı.`)
 })
