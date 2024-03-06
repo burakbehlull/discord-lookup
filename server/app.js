71ed686f0f8e5142ffe4c7e184f99bb9fdbf885d
burakbehlull <burakyabgu@gmail.com>
2024-03-06 15:01:38 +0300

    feat: Prepared POST request

--- server/app.js
+++ server/app.js
@@ -1,12 +1,26 @@
 const express = require('express')
 
 require('dotenv').config()
-const app = express()
-
 const {PORT} = process.env
 
-app.get('/api',(req,res)=> {
-    res.send('Merhaba API!')
+const app = express()
+
+app.post('/user', (req,res)=> {
+    const {userId} = req.body
+    
+    try {
+        res.status(200).json({
+            success: true,
+            message: "Kullanıcı bulundu.",
+            user: ''
+        })
+    } catch (err) {
+        res.status(204).json({
+            success: false,
+            message: "Kullanıcı bulunamadı.",
+            user: null
+        })
+    }
 })
 
 app.listen(process.env.PORT, ()=> {
