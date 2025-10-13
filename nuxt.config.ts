2ee7df878141e0e79ccc151a7d71f0fca2191f2f
burak behlül <burakyabgu@gmail.com>
2025-10-14 01:21:17 +0300

    chore: Integration tailwindcss and dais ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import daisyui from 'daisyui'
+
 export default defineNuxtConfig({
+  modules: ['@nuxtjs/tailwindcss'],
+	tailwindcss: {
+    cssPath: './app/main.css',
+	config: {
+		plugins: [daisyui()]
+	}
+  },
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
 })
